b4d084e6ec32b407f2932f8cd4542765e5e04c8d
sachaparahasti <sachhasti@gmail.com>
2026-01-11 20:21:13 +0530

    change

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -109,3 +111,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+// mysql.railway.internal
