1b50bd809a10649175e9263643086e67012ebca7
sachaparahasti <sachhasti@gmail.com>
2026-01-11 14:37:52 +0530

    chnage

--- server.js
+++ server.js
@@ -105,7 +105,6 @@ app.put(`${ROUTE}/:id`, (req, res) => {
 });
 
 // START SERVER (Render-safe)
-// server port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
